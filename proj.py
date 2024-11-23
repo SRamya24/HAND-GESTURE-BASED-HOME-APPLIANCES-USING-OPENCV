14f3574e9fd954994a8bc7da878418ae7ec2e0bd
SRamya24 <ramyasamynathan2405@gmail.com>
2024-11-23 19:18:51 +0530

    proj.py

--- proj.py
+++ proj.py
@@ -1,19 +1,15 @@
-import cv2 #install cv
-import mediapipe as mp # install mediapipe
+import cv2 # install cv
+import mediapipe as mp #install mediapipe
 import time
-
-import paho.mqtt.client as paho
-broker="broker.mqttdashboard.com"
+import paho.mqtt.client as paho #install paho.mqtt
+broker="broker.mqtt-dashboard.com"
 port=1883
-
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
-
 def on_publish(client,userdata,result):             #create function for callback
     print("data published \n")
     pass
-
 # For webcam input:
 cap = cv2.VideoCapture(0)
 with mp_hands.Hands(
@@ -26,34 +22,26 @@ with mp_hands.Hands(
       print("Ignoring empty camera frame.")
       # If loading a video, use 'break' instead of 'continue'.
       continue
-
     # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = hands.process(image)
-
+   # pass by reference.
+   image.flags.writeable = False
+   image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+   results = hands.process(image)
     # Draw the hand annotations on the image.
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
     # Initially set finger count to 0 for each cap
     fingerCount = 0
-
     if results.multi_hand_landmarks:
-
       for hand_landmarks in results.multi_hand_landmarks:
         # Get hand index to check label (left or right)
         handIndex = results.multi_hand_landmarks.index(hand_landmarks)
         handLabel = results.multi_handedness[handIndex].classification[0].label
-
         # Set variable to keep landmarks positions (x and y)
         handLandmarks = []
-
         # Fill list with x and y positions of each landmark
         for landmarks in hand_landmarks.landmark:
           handLandmarks.append([landmarks.x, landmarks.y])
-
         # Test conditions for each finger: Count is increased if finger is 
         #   considered raised.
         # Thumb: TIP x position must be greater or lower than IP x position, 
@@ -62,7 +50,6 @@ with mp_hands.Hands(
           fingerCount = fingerCount+1
         elif handLabel == "Right" and handLandmarks[4][0] < handLandmarks[3][0]:
           fingerCount = fingerCount+1
-
         # Other fingers: TIP y position must be lower than PIP y position, 
         #   as image origin is in the upper left corner.
         if handLandmarks[8][1] < handLandmarks[6][1]:       #Index finger
@@ -73,7 +60,6 @@ with mp_hands.Hands(
           fingerCount = fingerCount+1
         if handLandmarks[20][1] < handLandmarks[18][1]:     #Pinky
           fingerCount = fingerCount+1
-
         # Draw hand landmarks 
         mp_drawing.draw_landmarks(
             image,
@@ -82,19 +68,16 @@ with mp_hands.Hands(
             mp_drawing_styles.get_default_hand_landmarks_style(),
             mp_drawing_styles.get_default_hand_connections_style())
     # Display finger count
-    cv2.putText(image, str(fingerCount), (50, 450), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 0), 10)
-    fp=str(fingerCount)
-    # Display image
-    cv2.imshow('MediaPipe Hands', image)
-   
-    if(fingerCount!=0): 
-      client1= paho.Client("control1")                           #create client object
-      client1.on_publish = on_publish                          #assign function to callback
-      client1.connect(broker,port)   
-      ret= client1.publish("fingercountdata",fp)
-      print(fp)
-      time.sleep(1) 
-
-    if cv2.waitKey(5) & 0xFF == 27:
+    cv2.putText(image, str(fingerCount), (50, 450), cv2.FONT_HERSHEY_SIMPLEX,3, (255, 0, 0), 10)
+   fp=str(fingerCount)
+   # Display image
+   cv2.imshow('MediaPipe Hands', image)
+    client1= paho.Client("control1")                           #create client object
+    client1.on_publish = on_publish                          #assign function to callback
+    client1.connect("broker.mqtt-dashboard.com",port)                                 #establishconnection
+   ret= client1.publish("fingercountdata",fp)
+   print(fp)
+   time.sleep(1) 
+   if cv2.waitKey(5) & 0xFF == 27:
       break
 cap.release()
